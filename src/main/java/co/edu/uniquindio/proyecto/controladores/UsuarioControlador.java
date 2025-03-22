f29241862bd9fcdef5a626a0787367dd05786b36
DJRGB24 <kevin.payanenea@uqvirtual.edu.co>
2025-03-21 20:49:46 -0500

    Restructura Carpetas

--- src/main/java/co/edu/uniquindio/proyecto/controladores/UsuarioControlador.java
+++ src/main/java/co/edu/uniquindio/proyecto/controladores/UsuarioControlador.java
@@ -1,4 +1,4 @@
-package co.edu.uniquindio.controladores;
+package co.edu.uniquindio.proyecto.controladores;
 
 import co.edu.uniquindio.proyecto.dto.CrearUsuarioDTO;
 import co.edu.uniquindio.proyecto.dto.EditarUsuarioDTO;
---
+++ src/test/java/co/edu/uniquindio/proyecto/UsuarioControladorTest.java
@@ -0,0 +1,11 @@
+package co.edu.uniquindio.proyecto;
+
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class UsuarioControladorTest {
+
+
+
+}
