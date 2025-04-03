0f99e8b12396df61a13a1d5b777bc361b00a1e67
lABetancourtl <anderson.betancourta@uqvirtual.edu.co>
2025-04-02 23:56:59 -0500

    correccion de mas errores

--- src/main/java/co/edu/uniquindio/proyecto/modelo/vo/Ubicacion.java
+++ src/main/java/co/edu/uniquindio/proyecto/modelo/vo/Ubicacion.java
@@ -2,13 +2,12 @@ package co.edu.uniquindio.proyecto.modelo.vo;
 
 import lombok.*;
 
-
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
 @Builder
 public class Ubicacion {
-
     private double latitud;
     private double longitud;
 }
