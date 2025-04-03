cacbe9c3ad413f455d8b2da1418d124b252c9c4e
Kevin Payanene Ardila <zpayitaz@gmail.com>
2025-04-02 21:09:14 -0500

    Cambios a DTO

--- src/main/java/co/edu/uniquindio/proyecto/dto/CrearUsuarioDTO.java
+++ src/main/java/co/edu/uniquindio/proyecto/dto/CrearUsuarioDTO.java
@@ -2,30 +2,26 @@ package co.edu.uniquindio.proyecto.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class CrearUsuarioDTO {
+public record CrearUsuarioDTO(
     @NotBlank(message = "El nombre es obligatorio")
-    private String nombre;
+    String nombre,
     
     @NotBlank(message = "El email es obligatorio")
     @Email(message = "El formato del email no es válido")
-    private String email;
+    String email,
     
     @NotBlank(message = "La contraseña es obligatoria")
-    private String password;
+    String password,
     
     @NotBlank(message = "El teléfono es obligatorio")
-    private String telefono;
+    String telefono,
     
     @NotBlank(message = "La ciudad es obligatoria")
-    private String ciudad;
+    String ciudad,
     
     @NotBlank(message = "La dirección es obligatoria")
-    private String direccion;
+    String direccion
+    ){
+
 }
