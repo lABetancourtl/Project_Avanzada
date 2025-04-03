9564cf09eee393360d93ae8247cfbfd9bd094609
lABetancourtl <anderson.betancourta@uqvirtual.edu.co>
2025-04-02 23:56:59 -0500

    correccion de mas errores

--- src/main/java/co/edu/uniquindio/proyecto/mapper/ReporteMapper.java
+++ src/main/java/co/edu/uniquindio/proyecto/mapper/ReporteMapper.java
@@ -2,7 +2,9 @@ package co.edu.uniquindio.proyecto.mapper;
 
 import co.edu.uniquindio.proyecto.dto.CrearReporteDTO;
 import co.edu.uniquindio.proyecto.dto.ReporteDTO;
+import co.edu.uniquindio.proyecto.dto.UbicacionDTO;
 import co.edu.uniquindio.proyecto.modelo.documents.Reporte;
+import co.edu.uniquindio.proyecto.modelo.vo.Ubicacion;
 import org.bson.types.ObjectId;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -10,11 +12,13 @@ import org.mapstruct.Mapping;
 @Mapper(componentModel = "spring")
 public interface ReporteMapper {
 
+    @Mapping(target = "ubicacion", source = "ubicacion")
     Reporte toDocument(CrearReporteDTO dto);
-    //  ReporteDTO toDTO(Reporte reporte);
+
+    // Método adicional para mapear UbicacionDTO a Ubicacion
+    Ubicacion toUbicacion(UbicacionDTO ubicacionDTO);
 
     default ObjectId map(String value) {
         return new ObjectId(value);
     }
-
 }
