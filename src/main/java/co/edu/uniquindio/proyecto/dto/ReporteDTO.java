da40d4aa1eb17be7a0605f00eb936c59427a2745
Stevenz02 <jssmorales51@gmail.com>
2025-05-11 00:54:11 -0500

    Update ReporteDTO.java
    Ajuste reporteDTO estadoActual

--- src/main/java/co/edu/uniquindio/proyecto/dto/ReporteDTO.java
+++ src/main/java/co/edu/uniquindio/proyecto/dto/ReporteDTO.java
@@ -1,9 +1,5 @@
 package co.edu.uniquindio.proyecto.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -11,7 +7,7 @@ public record ReporteDTO (
         String id,
         String titulo,
         String descripcion,
-        String estado,
+        String estadoActual,
         LocalDateTime fecha,
         boolean importante,
         String clienteId,
