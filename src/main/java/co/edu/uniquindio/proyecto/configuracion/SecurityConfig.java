419a3bc5aa6c1ecb048d7b756507a79b317cadb0
Stevenz02 <jssmorales51@gmail.com>
2025-05-24 20:40:46 -0500

    fix connection#2
    fix

--- src/main/java/co/edu/uniquindio/proyecto/configuracion/SecurityConfig.java
+++ src/main/java/co/edu/uniquindio/proyecto/configuracion/SecurityConfig.java
@@ -69,7 +69,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("https://reporte-ciudadanos.web.app")); // Origen FRONT
+        config.setAllowedOriginPatterns(List.of("https://reporte-ciudadanos.web.app")); // Origen FRONT
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
