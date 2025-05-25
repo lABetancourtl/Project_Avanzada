aa1140b10233a11c43a8e686dd6cb4d40043f135
Anderson Betancourt Arenas <111938729+lABetancourtl@users.noreply.github.com>
2025-05-24 19:13:19 -0500

    aa
    aa

--- build.gradle.kts
+++ build.gradle.kts
@@ -58,6 +58,7 @@ dependencies {
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
     implementation("org.json:json:20240303")
+    implementation("org.mongodb:mongodb-driver-sync:5.3.0")
 }
 
 
